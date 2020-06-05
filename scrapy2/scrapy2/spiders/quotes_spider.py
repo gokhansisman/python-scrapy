47fa40aa72863d617eb75f97814941cb15290a3f
Gökhan Şişman <pa1ngokhan@gmail.com>
2020-06-06 00:08:40 +0300

    pagination

--- scrapy2/scrapy2/spiders/quotes_spider.py
+++ scrapy2/scrapy2/spiders/quotes_spider.py
@@ -3,8 +3,9 @@ from ..items import Scrapy2Item
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
+    page_number = 2
     start_urls = [
-        'http://quotes.toscrape.com/'
+        'http://quotes.toscrape.com/page/1/'
     ]
 
     def parse(self,response):
@@ -22,4 +23,10 @@ class QuoteSpider(scrapy.Spider):
             items['author'] = author
             items['tag'] = tag
             yield items
+        
+        next_page = 'http://quotes.toscrape.com/page/'+ str(QuoteSpider.page_number)+'/'
+
+        if QuoteSpider.page_number < 11:
+            QuoteSpider.page_number += 1
+            yield response.follow(next_page,callback=self.parse)
         
